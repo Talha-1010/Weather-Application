05e55e8501fbd8daf3fe7dd53dcb0c1ae84c4cfd
Talha-1010 <talhanasr3e@gmail.com>
2021-08-18 16:11:44 +0500

    removed clear button and added remove button for each card

--- script.js
+++ script.js
@@ -9,7 +9,7 @@
         button.addEventListener("click",function(){
 
             var city = document.getElementById("name").value;
-            fetch("http://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=2743020fa1a091d4c73c116dc96505f7")
+        fetch("http://api.openweathermap.org/data/2.5/weather?q="+city+"&units=metric&appid=2743020fa1a091d4c73c116dc96505f7")
         .then(response => response.json())
         .then(
             function(data){
@@ -19,16 +19,29 @@
                 var place = document.getElementById("place");
                 card.style = "width: 18rem";
                 card.className="card";
-
-                var img = document.createElement("img");
-                img.className="card-img-top";
-                img.src= "https://i.redd.it/d52iak5b8jn51.jpg";
-                img.alt = "card image cap";
-                card.appendChild(img);
-
+                card.id = "card";
+                
 
                 var cardBody = document.createElement("div");
                 cardBody.className = "card-body";
+
+
+                var figure = document.createElement('figure');
+
+
+                var icon = document.createElement("img");
+                icon.src = "http://openweathermap.org/img/wn/"+data.weather[0].icon+"@2x.png";
+                icon.subtit
+                icon.style = "position: relative; left: 10vw";
+                figure.appendChild(icon);
+                
+                var figcaption = document.createElement('figcaption');
+                figcaption.innerHTML = "temp "+ data.main.temp +"°C" + " humidity " + data.main.humidity + " max temp " + data.main.temp_max +"°C" + " min temp " + data.main.temp_min + "°C";
+                figure.appendChild(figcaption);
+                
+                cardBody.appendChild(figure);
+
+
 
                 var city = document.createElement("h5");
                 city.innerHTML= data.name;
@@ -40,28 +53,28 @@
                 desc.className ="card-text";
                 cardBody.appendChild(desc);
 
-                var icon = document.createElement("img");
-                icon.src = "http://openweathermap.org/img/wn/"+data.weather[0].icon+"@2x.png";
-                cardBody.appendChild(icon);
+                
+
+                var removeButton = document.createElement('button');
+                removeButton.href= "#";
+                removeButton.id = "remove";
+                removeButton.classList="btn btn-primary";
+                removeButton.innerHTML="remove";
+                cardBody.appendChild(removeButton);
+
+                removeButton.addEventListener("click",function(){
+                        $("#card").remove();
+                })
+
 
                 card.appendChild(cardBody);
                 place.appendChild(card);
-
-
-                
-
 
             }
         )
         .catch(err => console.error(err));
         })
 
-        var clearButton = document.getElementById("clear");
-
-        clearButton.addEventListener("click",function(){
-           
-            $("#place").empty();
-        })
 
 
 })();
